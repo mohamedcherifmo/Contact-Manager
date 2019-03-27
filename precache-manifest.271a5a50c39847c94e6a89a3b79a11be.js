8fb49afd38e2c30117082292e86658c76ac3ac51
mohamedcherifmo <mohamed.cherif@outlook.com>
2019-03-27 11:47:35 -0400

    Updates

---
+++ precache-manifest.271a5a50c39847c94e6a89a3b79a11be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d43fcecf4928b0c254b",
+    "url": "/Contact-Manager/static/js/runtime~main.e499c403.js"
+  },
+  {
+    "revision": "2bad9caf5bdf33fbbd34",
+    "url": "/Contact-Manager/static/js/main.ea141538.chunk.js"
+  },
+  {
+    "revision": "58a5b5e46a67423c9fb9",
+    "url": "/Contact-Manager/static/js/2.3411ee79.chunk.js"
+  },
+  {
+    "revision": "2bad9caf5bdf33fbbd34",
+    "url": "/Contact-Manager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "58a5b5e46a67423c9fb9",
+    "url": "/Contact-Manager/static/css/2.f2eb8eea.chunk.css"
+  },
+  {
+    "revision": "a6dba720ac46b32c7cd326d9455418ff",
+    "url": "/Contact-Manager/index.html"
+  }
+];
